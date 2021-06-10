99a63b54ba11082ea111069f9257d60af223844b
Jack Naisbett <jack.naisbett@gmail.com>
2021-06-10 22:48:05 +0100

    Update dockerfile and change entry point to script for constant run Add cron support

---
+++ app/entry.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -f /config/octograph.ini ]; then
+	python ./octopus_to_influxdb.py
+else
+	echo "/config/octograph.ini is missing, exiting"
+	exit 1
+fi
+
+if [ -f /config/crontab ]; then
+	cp /config/crontab /etc/cron.d/crontab
+	chmod 0644 /etc/cron.d/crontab
+	crontab /etc/cron.d/crontab
+	cron -f
+else
+	echo "/config/crontab is missing, exiting"
+	exit 1
+fi
