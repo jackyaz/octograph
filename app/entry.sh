0a5bd04b8fa6ff62e6cd35431a94fb8b5f526517
Jack Naisbett <jack.naisbett@gmail.com>
2021-06-10 23:55:06 +0100

    Correct filename for example crontab

--- app/entry.sh
+++ app/entry.sh
@@ -11,7 +11,7 @@ if [ -f /config/crontab ]; then
 	cp /config/crontab /etc/cron.d/crontab
 else
 	echo "/config/crontab is missing, using default schedule"
-	cp /octograph/crontab /etc/cron.d/crontab
+	cp /octograph/example-crontab /etc/cron.d/crontab
 fi
 
 chmod 0644 /etc/cron.d/crontab
