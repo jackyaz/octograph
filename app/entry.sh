1edb79f4e065181532c2e5c1ec590857743f6a48
Jack Naisbett <jack.naisbett@gmail.com>
2021-06-12 12:32:34 +0100

    Correct error message

--- app/entry.sh
+++ app/entry.sh
@@ -3,7 +3,7 @@
 if [ -f /octograph/config/octograph.ini ]; then
 	python /octograph/octopus_to_influxdb.py
 else
-	echo "/config/octograph.ini is missing, exiting"
+	echo "/octograph/config/octograph.ini is missing, exiting"
 	exit 1
 fi
 
